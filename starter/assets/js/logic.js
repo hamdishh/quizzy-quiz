e537835056ef47b71beb65bba53317b98026beca
Hamdi <hamdishh2003@gmail.com>
2024-02-15 12:17:54 +0000

    set function to start quiz and using getElementbyId hiding start screen element once user begins wuiz

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -5,3 +5,14 @@ const choicesContainer = document.getElementById('choices');
 let currentQuestion = 0;
 let score= 0;
 let timeRemaining = 60;
+
+//Set function to start the quiz
+function startQuiz() {
+ // Ensure start screen is hidden once user starts the quiz so only questions will render
+document.getElementById('start-screen').classList.add('hide');
+// Display the first question
+displayQuestion(questions[currentQuestion]);
+    // Start the timer
+    startTimer();
+  }
+  
